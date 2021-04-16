be8a6a820bd700ced65faa980c31c771167a1aa6
tmem11 <tmem.nsasra@gmail.com>
2021-04-16 18:51:33 +0300

    add features

--- code.c
+++ code.c
@@ -3,6 +3,29 @@
 #include<unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#define MAXLENGTH 1000
+#define MAXARR 10
+
+
+
+void printCurrentPath(){
+    char * currentPath = getenv("PWD");
+    char * userName = getenv("USER");
+    printf(" %s:%s >> ",userName,currentPath);
+
+}
+void execArgs(char* input,char userCmd[MAXARR][MAXLENGTH], int* length){
+    char *found;
+    int i=0;
+
+    while( (found = strsep(&input," ")) != NULL ) {
+        strcpy(userCmd[i], found);
+        i++;
+      //  printf("%d",i);
+    }
+    length=&i;
+   
+}
 
 
 int main (int argc, char **argv)
@@ -10,30 +33,51 @@ int main (int argc, char **argv)
 
     printf("------------- starting custom shell --------------------- \n");
     printf("------------- -------------------- --------------------- \n");
-    while (1)
-    {
-        char  userInput [1024] ;
-        char * currentPath = getenv("PWD");
-        char * userName = getenv("USER");
-        printf(" %s:%s >> ",userName,currentPath);
-        scanf("%s", userInput );
+    while (1) {
+        int length = 0;
+        printCurrentPath();
+        char userInput[MAXLENGTH];
+        char cmd[MAXARR][MAXLENGTH];
+
+        scanf("%s", userInput);
+        execArgs(userInput, cmd, &length);
+
+        if (fork() == 0) {
+        if (strcmp(userInput, "pwd") == 0) {
 
 
-             if(strcmp(userInput,"pwd") == 0)
-            {
-                 if(fork()==0) {
+                if (execlp("pwd", "pwd", NULL) == -1) {
+                    printf("\nCould not execute command..");
 
-                    if (execlp("pwd", "pwd", NULL) == -1) {
-                        printf("\nCould not execute command..");
-
-                    }
-                    exit(0);
                 }
-                 else{
-                     wait(NULL);
-                 }
+                exit(0);
+            }
+            if (strcmp(userInput, "whoami") == 0) {
 
-             }
+
+                if (execlp("whoami", "whoami", NULL) == -1) {
+                    printf("\nCould not execute command..");
+
+                }
+                exit(0);
+            }
+            if (strcmp(cmd[0], "ls") == 0) {
+
+
+                char *binaryPath = "/bin/ls";
+                char *arg1 = "-lh";
+                char *arg2 = "/home";
+                
+ 
+            execl(binaryPath, binaryPath, arg1, arg2, NULL);
+                exit(0);
+            }
+        
+
+    }
+        else {
+            wait(NULL);
+        }
 
 
 
